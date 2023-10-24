334496191c669d5fd1fff38ae2be8a83f3b895b5
Prathap-Siva <prathapsendto@gmail.com>
2023-10-24 21:13:02 +0530

    new file:   countryFlagList.js 	new file:   countryName&Region.js 	new file:   objectsSameOrNot.js

---
+++ countryName&Region.js
@@ -0,0 +1,15 @@
+let XMLHttpRequest = require('xhr2')
+
+let xhr = new XMLHttpRequest();
+
+xhr.open("GET", 'https://restcountries.com/v3.1/all');
+
+xhr.onload = function() {
+    let datas = JSON.parse(xhr.responseText)
+
+    for(let data of datas){
+    
+        console.log('Country Name: '+ data.name.common+',',  'Region: '+ data.region+',',  'Subregion: '+data.subregion)
+    }
+}
+xhr.send();
---
+++ objectsSameOrNot.js
@@ -0,0 +1,22 @@
+let obj1 = { name: 'Person 1', age: 5}
+let obj2 = { age: 5, name: 'Person 1'}
+
+let obj1Keys = Object.keys(obj1)
+let obj2Keys = Object.keys(obj2)
+
+let flag= true;
+
+if(obj1Keys.length == obj2Keys.length){
+    for(let key in obj1){
+        if(obj1[key] != obj2[key]){
+            flag = false
+        }
+    }
+}else{
+    flag = false;
+}
+if(flag){
+    console.log('Object Keys & Values are same');
+}else{
+    console.log('Object Keys & Values are NOT same');
+}
